a216daa588dce5b413c60070c45e88a9ff90205e
Matteo Furgani <151537051+MatteoFurgani@users.noreply.github.com>
2024-05-28 23:38:03 +0200

    modifica foto

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -59,20 +59,20 @@ const NavBar = () => {
       setIsLoggedIn(true);
     }
 
-    const handleScroll = () => {
-      const navbar = document.getElementById("main-navbar");
-      if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-      } else {
-        navbar.classList.remove("scrolled");
-      }
-    };
+    // const handleScroll = () => {
+    //   const navbar = document.getElementById("main-navbar");
+    //   if (window.scrollY > 50) {
+    //     navbar.classList.add("scrolled");
+    //   } else {
+    //     navbar.classList.remove("scrolled");
+    //   }
+    // };
 
-    window.addEventListener("scroll", handleScroll);
+    // window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    // return () => {
+    //   window.removeEventListener("scroll", handleScroll);
+    // };
   }, []);
 
   ////////////////////////////////////REGISTRAZONE////////////////////////////////
@@ -124,7 +124,7 @@ const NavBar = () => {
       <Navbar expand="lg" className="navbar-custom" id="main-navbar">
         <Container fluid>
           <Link to="/" className="navbar-brand">
-            <img src="../../public/logo-toeletta.png" alt="logo" width="50" />
+            <img src="/public/logo-toeletta.png" alt="logo" width="50" />
           </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse
